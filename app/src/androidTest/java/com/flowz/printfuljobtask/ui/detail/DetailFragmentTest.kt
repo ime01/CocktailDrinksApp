fcdcee36046ce2d40fb322df1ae89cfe7722cac1
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-02-04 17:53:32 -0800

    submit

---
+++ app/src/androidTest/java/com/flowz/printfuljobtask/ui/detail/DetailFragmentTest.kt
@@ -0,0 +1,17 @@
+package com.flowz.printfuljobtask.ui.detail
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.flowz.printfuljobtask.MainActivity
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DetailFragmentTest{
+
+//    @Rule
+//    @JvmField
+//    var mActivityTestRule = ActivityScenarioRule(DetailFragmentTest::class.java)
+
+}
